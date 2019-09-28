9385d86111a367982b2063625c9ccddddccaa475
Federico Grandi <fgrandi30@gmail.com>
2019-09-28 15:52:10 +0200

    TEST

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const fs = require('fs');
-const path = require('path').join(__dirname, './changed_file.txt');
-
-console.log("RUNNING")
-
-fs.writeFileSync(path, (new Date()) + '');
-
-console.log('ENDED')
+console.log(process.env.DEFAULT_BRANCH)
