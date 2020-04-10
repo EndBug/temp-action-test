0d2c59d381f3b19a55bb3d9433a3ea960b97a1cd
Federico Grandi <fgrandi30@gmail.com>
2020-04-10 12:03:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 const email = process.env.UBI_EMAIL,
       password = process.env.UBI_PASSWORD;
 
-console.log(email)
-console.log(password)
-console.log(!email || !password)
+console.log(!!email)
+console.log(!!password)
+if (!email || !password) throw new Error('Cannot update file without API credentials');
