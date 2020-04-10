0707a97e84978584adfd13d31f59dc5ab06d4b52
Federico Grandi <fgrandi30@gmail.com>
2020-04-10 12:01:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const {UBI_EMAIL, UBI_PASSWORD} = process.env
-console.log(UBI_EMAIL)
-console.log(UBI_PASSWORD)
-console.log(!UBI_EMAIL || !UBI_PASSWORD)
+require('dotenv').config()
+const email = process.env.UBI_EMAIL,
+      password = process.env.UBI_PASSWORD;
+
+console.log(email)
+console.log(password)
+console.log(!email || !password)
